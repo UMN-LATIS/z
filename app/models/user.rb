be90590df2d2ce7aa6f944ba03f29999f4dbdb3a
wozniak <wozniak@umn.edu>
2016-05-24 16:09:58 -0500

    fixed join table names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :groups, join_table: GroupsUser
+  has_and_belongs_to_many :groups, join_table: :groups_users
   # this next bit of magic removes any associations in the group_users table
   before_destroy {|user| user.groups.clear}
   belongs_to :group, foreign_key: 'context_group_id'
