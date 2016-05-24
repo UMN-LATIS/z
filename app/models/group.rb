7add4dad99d78270505da7eb00a4d140cd0d2066
andersen <andersen@umn.edu>
2016-05-24 16:04:12 -0500

    Nullify on removal of group

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,8 @@
 class Group < ApplicationRecord
-  has_and_belongs_to_many :users, join_table: GroupsUser
+  
+  has_and_belongs_to_many :users, join_table: :groups_users
   # this next bit of magic removes any associations in the group_users table
   before_destroy {|group| group.users.clear}
+  
+  has_many :users, foreign_key: :context_group_id, dependent: :nullify
 end
