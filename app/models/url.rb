2ffba80996bc0f57d3e78c3159f71b048708332e
andersen <andersen@umn.edu>
2016-05-25 17:58:10 -0500

    Preliminary ajax url creation

--- app/models/url.rb
+++ app/models/url.rb
@@ -14,10 +14,15 @@
 
 class Url < ApplicationRecord
   belongs_to :group
+  has_many :clicks
   has_and_belongs_to_many :transfer_requests, join_table: :transfer_request_urls
+  
   # this next bit of magic removes any associations in the transfer_request_urls table
   before_destroy {|url| url.transfer_requests.clear}
-  has_many :transfer_requests, :dependent => :destroy
-  has_many :clicks
+  
+  before_validation(on: :create) do
+    self.group = User.first.context_group
+    self.total_clicks = 0
+  end
 
 end
