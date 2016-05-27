e2759a1df53ea4e8a193e737678b17e61d816ea1
andersen <andersen@umn.edu>
2016-05-27 14:04:00 -0500

    Best practices

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,16 +9,18 @@
 #  created_at       :datetime         not null
 #  updated_at       :datetime         not null
 #
-
 class User < ApplicationRecord
   has_and_belongs_to_many :groups, join_table: :groups_users
   # this next bit of magic removes any associations in the group_users table
-  before_destroy {|user| user.groups.clear}
-  belongs_to :context_group, foreign_key: 'context_group_id', class_name: "Group"
-  
+  before_destroy { |user| user.groups.clear }
+  belongs_to :context_group,
+             foreign_key: 'context_group_id',
+             class_name: 'Group'
+
   before_validation(on: :create) do
-    self.context_group = Group.create(name: self.uid, description: self.uid) if self.context_group.blank?
-    self.groups << self.context_group
+    self.context_group =
+      Group.create(name: uid, description: uid) if context_group.blank?
+    groups << context_group
   end
-  
+
 end
