6dd69d29d3e306ebaff12298d841cc5a5f09afd4
tdbedner <tdbedner@umn.edu>
2016-05-29 19:42:17 -0500

    Adding nav bar and sticky footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # Return true if a user is logged in through shibboleth. 
+  def logged_in?
+#    if current_agent.blank? or current_agent.internet_id == "anonymous"
+#      false
+#    else 
+#      true
+#    end
+false
+  end
+
 end
