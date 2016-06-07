d16868836f56267b329c7c9a83c60967e4c65815
wozniak <wozniak@umn.edu>
2016-06-07 10:42:29 -0500

    just starting froups methods

--- app/models/group.rb
+++ app/models/group.rb
@@ -10,10 +10,27 @@
 #
 
 class Group < ApplicationRecord
-  
   has_and_belongs_to_many :users, join_table: :groups_users
   # this next bit of magic removes any associations in the group_users table
-  before_destroy {|group| group.users.clear}
-  
-  has_many :users, foreign_key: :context_group_id, dependent: :nullify
+  before_destroy { |group| group.users.clear }
+
+  # has_many :context_user, foreign_key: :context_group_id, dependent: :nullify, source: :user
+
+  def has_user user
+    users.exists?(user.id)
+  end
+
+  def add_user(user, send_notifications=0)
+    self.users << user unless users.exists?(user.id)
+  end
+
+  def remove_user user
+    users.delete(user) unless !has_user(user)
+  end
+
+  def set_as_current_context user
+    user.context_group = self
+  end
+
+
 end
