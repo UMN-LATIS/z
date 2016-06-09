54e740dd2a149082defe05e82de4e40ca6fb1ca3
wozniak <wozniak@umn.edu>
2016-06-09 15:13:41 -0500

    following Ryan's second run at url CRUD for groups

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -9,9 +9,10 @@ class GroupsController < ApplicationController
   end
 
   def show
+    @group_identifier = @group.id
     respond_to do |format|
       format.html
-      format.js   { render :layout => false }
+      format.js   { render layout: false }
     end
   end
 
@@ -32,15 +33,19 @@ class GroupsController < ApplicationController
       if @group.save
         format.html { redirect_to groups_path, notice: 'Group was successfully created.' }
         format.json { render :show, status: :created, location: @group }
+        format.js   { render :show }
       else
         format.html { render :new }
         format.json { render json: @group.errors, status: :unprocessable_entity }
+        format.js   { render :edit }
       end
-      format.js   { render :layout => false }
+
     end
+
   end
 
   def edit
+    @group_identifier = @group.id
     respond_to do |format|
       format.html
       format.js   { render :layout => false }
@@ -48,18 +53,18 @@ class GroupsController < ApplicationController
   end
 
   def update
-    @group_identifier = @group.id
-
     respond_to do |format|
       if @group.update(group_params)
         format.html { redirect_to @group, notice: 'Group was successfully updated.' }
         format.json { render :show, status: :ok, location: @group }
+        format.js   { render :show }
       else
         format.html { render :edit }
         format.json { render json: @group.errors, status: :unprocessable_entity }
+        format.js   { render :edit }
       end
-      format.js   { render :layout => false }
     end
+
   end
 
   def destroy
@@ -76,6 +81,7 @@ class GroupsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_group
     @group = Group.find(params[:id])
+    @group_identifier = @group.id
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
