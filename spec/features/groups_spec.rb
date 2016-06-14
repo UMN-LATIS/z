bcadcdd0c79eecbd0bf8ca5e5c175f35a5d2a46a
wozniak <wozniak@umn.edu>
2016-06-14 09:20:06 -0500

    adding "Groups" option to nav bar thingy

---
+++ spec/features/groups_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe 'groups index page' do
+  before { @user = FactoryGirl.create(:user) }
+
+
+  describe 'displaying the groups' do
+
+    describe 'should display name' do
+
+    end
+
+    describe 'should display description' do
+
+    end
+
+    describe 'should display the created at date' do
+
+    end
+
+  end
+
+  describe 'updating a group' do
+    describe 'group name should be editable' do
+
+    end
+    describe 'group description should be editable' do
+
+    end
+    describe 'group created at should not be editable' do
+
+    end
+    describe 'should save to the database' do
+
+    end
+  end
+
+  describe 'deleting an existing group' do
+    describe 'should delete the group' do
+      describe
+    end
+  end
+
+  describe 'creating a new group' do
+
+  end
+
+end
