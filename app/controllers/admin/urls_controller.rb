cda5187ea3a659ec403717e9c08be999e3bf9cf5
andersen <andersen@umn.edu>
2016-07-19 09:50:57 -0500

    Adding admin URL filtering and editing

--- app/controllers/admin/urls_controller.rb
+++ app/controllers/admin/urls_controller.rb
@@ -1,29 +1,64 @@
 class Admin::UrlsController < ApplicationController
-	def index
-		#code
-	end
+  def index
+    # Find users by x500
+    keyword_to_search = "%#{params[:url_keyword].try(:downcase)}%"
+    owner_to_search = "%#{params[:url_owner]}%"
+    possible_groups = Group.where('name LIKE ?', owner_to_search).map(&:id)
 
-	def show
-		#code
-	end
+    @urls = Url.where('keyword LIKE ? and group_id IN (?)', keyword_to_search, possible_groups)
+  end
 
-	def new
-		#code
-	end
+  # GET /urls/1
+  # GET /urls/1.json
+  def show
+    @url_identifier = @url.id
+    respond_to do |format|
+      format.html
+      format.js { render layout: false }
+   end
+  end
 
-	def create
-		#code
-	end
+  def edit
+    @url_identifier = @url.id
+    respond_to do |format|
+      format.html
+      format.js { render layout: false }
+   end
+  end
 
-	def edit
-		#code
-	end
+  # PATCH/PUT /urls/1
+  # PATCH/PUT /urls/1.json
+  def update
+    respond_to do |format|
+      if @url.update(url_params)
+        format.json { render :show, status: :ok, location: @url }
+        format.js   { render :show }
+      else
+        format.json { render json: @url.errors, status: :unprocessable_entity }
+        format.js   { render :edit }
+      end
+    end
+  end
 
-	def update
-		#code
-	end
+  # DELETE /urls/1
+  # DELETE /urls/1.json
+  def destroy
+    @url.destroy
+    respond_to do |format|
+      format.json { head :no_content }
+      format.js   { render layout: false }
+    end
+  end
 
-	def destroy
-		#code
-	end
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_url
+      @url = Url.find(params[:id])
+      @url_identifier = @url.id
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def url_params
+      params.require(:url).permit(:url, :keyword, :group_id, :modified_by)
+    end
 end
