03ef7a9bef42c158319d328c7efd39641f116068
wozniak <wozniak@umn.edu>
2016-08-23 10:13:42 -0500

    work on tests completed

--- spec/features/group_membership_spec.rb
+++ spec/features/group_membership_spec.rb
@@ -3,41 +3,56 @@ require 'rails_helper'
 
 describe 'groups members index page' do
   before do
-    @user = FactoryGirl.create(:user)
+    @user = FactoryGirl.create(:user, uid:'wozniak')
     sign_in(@user)
+    group.users << @user
+    group.save
+    visit group_members_path(group)
   end
 
-
-  describe 'creating and deleting a group member', js: true do
-    describe 'to an existing group' do
-      let(:group) { FactoryGirl.create(:group) }
-      before do
-        group.users << @user
-        group.save
-        sign_in @user
-        visit group_members_path(group)
+  describe 'visiting the group membership page' do
+    let(:group) { FactoryGirl.create(:group) }
+    let(:user) {User.where(:uid => @user.uid).first}
+    describe 'page content' do
+      it 'should display the group name' do
+        expect(page).to have_content group.name
       end
-
-      it 'adding should increase the user count of the group by 1' do
-        fill_in 'people_search', :with => 'andersen'
-        expect do
-          click_button 'Add'
-          wait_for_ajax
-        end.to change(group.users, :count).by(1)
-
+      it 'should display the group member uid' do
+        expect(page).to have_content @user.uid
       end
-
-      it 'deleting should decrease the number of group users by one' do
-        expect do
-          visit group_member_path(group, @user)
-          puts page.body
-          delete :destroy, {:group_id => group.id, :id => @user.id}
-          wait_for_ajax
-        end.to change(group.users, :count).by(-1)
-
+      it 'should display the group member full name' do
+        expect(page).to have_content user.user_full_name
+      end
+      it 'should display the group member email' do
+        expect(page).to have_content user.email
+      end
+      it 'should display a delete button' do
+        expect(page).to have_content 'Delete'
       end
     end
   end
 
+  describe 'creating and deleting a group member', js: true do
+    describe 'to an existing group' do
+      let(:group) { FactoryGirl.create(:group) }
+      it 'adding should increase the user count of the group by 1' do
+        expect do
+          find("#uid", :visible => false).set 'andersen'
+          click_button 'Add'
+          wait_for_ajax
+          group.reload
+        end.to change(group.users, :count).by(1)
 
+      end
+      describe 'clicking delete' do
+        it 'should delete the group member' do
+          expect do
+            find('.delete-group-member').click
+            wait_for_ajax
+          end.to change(group.users, :count).by(-1)
+        end
+      end
+
+    end
+  end
 end
