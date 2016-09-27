786179a21580811e25dabd539cdb8d4b7b0b11bf
Judy Kallestad <kalle001@x-10-104-37-19.uofm-secure.wireless.umn.edu>
2016-09-27 14:46:14 -0500

    new lock file bro

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.umn.edu:latis-sw/z.git'
 set :rbenv_ruby, File.read('.ruby-version').strip
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/swadm/web/z/'
