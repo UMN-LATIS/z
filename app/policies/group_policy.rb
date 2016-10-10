879be0bfa55485535e7c476969a00f76f45fd888
wozniak <wozniak@umn.edu>
2016-10-10 14:12:01 -0500

    policies and the controller calls that need them.

--- app/policies/group_policy.rb
+++ app/policies/group_policy.rb
@@ -1,24 +1,24 @@
 class GroupPolicy < ApplicationPolicy
 
   def index?
-    user_has_access
+    user_has_access?
   end
 
   def show?
-    user_has_access
+    user_has_access?
   end
 
   def update?
-    user_has_access
+    user_has_access?
   end
 
   def destroy?
-    user_has_access
+    user_has_access?
   end
 
   private
 
-  def user_has_access
+  def user_has_access?
     user.admin? || (record.is_a?(Group) && record.user?(user)) || record.all? { |rec| rec.user?(user) }
   end
 
