41551885d79fc5f79a6a41e80bb09640eb1f6180
wozniak <wozniak@umn.edu>
2016-11-17 13:37:32 -0600

    all tests fixed but 1

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,9 +34,6 @@ Capybara.javascript_driver = :poltergeist
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
-
-  config.include UrlHelper
-
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
