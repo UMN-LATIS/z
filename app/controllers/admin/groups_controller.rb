1830e16d6219fefa7755fcd37643bcae37cbe53d
Bernard Ferguson <bvfergus@umn.edu>
2017-01-13 12:17:46 -0600

    Groups admin controller

---
+++ app/controllers/admin/groups_controller.rb
@@ -0,0 +1,10 @@
+# controllers/groups_controller.rb
+class Admin::GroupsController < ApplicationController
+  # before_action :set_group, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_is_admin
+
+  def index
+    @groups = Group.all
+  end
+
+end
