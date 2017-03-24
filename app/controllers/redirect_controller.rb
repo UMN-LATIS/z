f9cea59e5904c585b0d1920c0f4899f1f4e0642f
andersen <andersen@umn.edu>
2017-03-24 13:34:45 -0500

    Streamline the redirect controller
    It has limited responsibility — simply load the URL and redirect. Don’t
    worry about loading any unnecessary stuff, like auth business, access
    methods, etc. Inherit from a base controller and only load what you
    need. Additionally, skip the callbacks/validations (using
    update_columns) on the URL since we are only updating the total_clicks.

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -1,14 +1,11 @@
-class RedirectController < ApplicationController
-
-  skip_before_action :ping_lookup_service
-
+class RedirectController < ActionController::Base
   def index
-    url = Url.find_by_keyword(params[:keyword])
-    if url.present?
+    url = Url.find_by(keyword: params[:keyword])
+    if url.nil?
+      redirect_to root_path
+    else
       url.add_click!(request.location)
       redirect_to(url.url)
-    else
-      redirect_to root_path
     end
   end
 end
