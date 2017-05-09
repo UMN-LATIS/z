d6b2eabcdbeae4a09cb2603b50def6551e8ecd92
andersen <andersen@umn.edu>
2017-05-09 13:28:24 -0500

    Update actions

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -2,7 +2,7 @@ $(document).bind('turbolinks:load', function () {
   var userTable = $('#groups-table').DataTable({
      "pageLength": 25,
 		 "autoWidth": false,
-     "order": [2, "desc"],
+     "order": [3, "desc"],
      "columnDefs": [ {
       "targets": 4,
       "orderable": false
