cd5810ce7697fcf0cce6679f94b3ccd33005df31
andersen <andersen@umn.edu>
2017-05-17 10:09:00 -0500

    Adding better empty table language

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,5 +1,8 @@
 $(document).bind('turbolinks:load', function () {
   var userTable = $('#groups-table').DataTable({
+     "language": {
+       "emptyTable": I18n.t('views.groups.index.table.empty')
+     },
      "pageLength": 25,
 		 "autoWidth": false,
      "order": [3, "desc"],
