acd8a890de01bbe5351627316c0f05326769d300
andersen <andersen@umn.edu>
2017-05-24 16:28:52 -0500

    Don't pass around an object -- keep it to primitives

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -5,7 +5,7 @@ class RedirectController < ActionController::Base
       redirect_to root_path
     else
       redirect_to(url.url)
-      url.add_click!(request.location)
+      url.add_click!(request.location.try(:country_code))
     end
   end
 end
