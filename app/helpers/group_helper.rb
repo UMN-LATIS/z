5b2d076817c7d6414cad04502a179e5f60df9522
Bernard Ferguson <bvfergus@umn.edu>
2017-05-30 09:02:03 -0500

    Construct Collection Switcher Select/Dropdown the same way in the table and on the stats page.

--- app/helpers/group_helper.rb
+++ app/helpers/group_helper.rb
@@ -3,4 +3,8 @@ module GroupHelper
     return t('views.urls.index.table.collection_filter.none') if group.default?
     group.name
   end
+
+  def group_names_and_ids_for_select(groups)
+    groups.collect { |group| [display_name(group), group['id'] ] }
+  end
 end
