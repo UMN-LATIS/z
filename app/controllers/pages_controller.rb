8d0a85b05ae263a29ff00b490f6913cf60ca8e87
Robert Wozniak <wozniak@umn.edu>
2017-05-30 12:22:08 -0500

    prevent exception and give a 404 instead.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,15 @@
 class PagesController < ApplicationController
   def show
-    render template: "pages/#{params[:page]}"
+    if valid_page?
+      render template: "pages/#{params[:page]}"
+    else
+      render file: "public/404.html", status: :not_found
+    end
   end
+
+  private
+  def valid_page?
+    File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
+  end
+
 end
