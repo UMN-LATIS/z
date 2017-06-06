49db91848418c7037068926efe3dec3dfa1d3397
andersen <andersen@umn.edu>
2017-06-05 22:16:51 -0500

    Add collection support

--- app/controllers/api/v1/urls_controller.rb
+++ app/controllers/api/v1/urls_controller.rb
@@ -1,13 +1,24 @@
 class Api::V1::UrlsController < Api::V1::BaseController
   def create
+    # Find group_id to use for new URL
+    if params[:collection_name]
+      @group_id = @current_user.groups.find_by(name: params[:collection_name]).try(:id)
+      if @group_id.blank?
+        render json: { error: 'Cannot find collection with that name.' }
+        return
+      end
+    else
+      @group_id = @current_user.default_group_id
+    end
+
     # Create URL
-    @url = Url.new(url: params[:url], group_id: @current_user.default_group_id)
+    @url = Url.new(url: params[:url], group_id: @group_id)
 
     # Return
     if @url.save
-      render json: view_context.full_url(@url).to_json
+      render json: { success: view_context.full_url(@url) }
     else
-      render json: { errors: @url.errors.full_messages }
+      render json: { error: @url.errors.full_messages }
     end
   end
 
