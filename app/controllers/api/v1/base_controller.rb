6e19028ec0830a095de2a08217b5a557f67bad6b
andersen <andersen@umn.edu>
2017-06-07 12:14:34 -0500

    Organize auth

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,11 +4,17 @@ class Api::V1::BaseController < ActionController::Base
   before_action :destroy_session, :api_authenticate
 
   def api_authenticate
-    auth_header = request.headers['Authorization']
-    user_uid, token = auth_header.split(':')
+    # Parse Auth Header
+    user_uid, token = request.headers['Authorization'].split(':')
+    head(:unauthorized) unless user_uid && token
+
+    # Load User
     @current_user = User.find_by(uid: user_uid)
+    head(:unauthorized) if @current_user.try(:sercet_key).blank?
+
+    # Decode Token
     begin
-      @payload = JWT.decode(token, @current_user.try(:secret_key).to_s, true, algorithm: 'HS256').first
+      @payload = JWT.decode(token, @current_user.try(:secret_key), true, algorithm: 'HS256').first
     rescue JWT::VerificationError, JWT::ExpiredSignature
       head(:unauthorized)
     end
@@ -17,4 +23,5 @@ class Api::V1::BaseController < ActionController::Base
   def destroy_session
     request.session_options[:skip] = true
   end
+
 end
