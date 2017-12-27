92f4000ff509d3e93e70b19770ad11354025419f
mcfa0086 <mcfa0086@umn.edu>
2017-12-27 12:49:41 -0600

    Feature/ignore googlebot (#489)
    Don't increment click counts for bot-generated clicks.  Use browser gem to determine bot, based on user agent.

--- spec/controllers/redirect_spec.rb
+++ spec/controllers/redirect_spec.rb
@@ -17,4 +17,25 @@ describe RedirectController do
       end
     end
   end
+
+  describe 'Click count /:keyword' do
+    describe 'user agent google' do
+      it 'should not count' do
+        initialClicks = url.total_clicks
+        @request.user_agent = "Googlebot/2.1"
+        get :index, keyword: url.keyword
+        newURL = Url.find(url.id)
+        expect(url.total_clicks).to eq(newURL.total_clicks)
+      end
+    end
+    describe 'user agent chrome' do
+      it 'should count' do
+        initialClicks = url.total_clicks
+        @request.user_agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+        get :index, keyword: url.keyword
+        newURL = Url.find(url.id)
+        expect(url.total_clicks).not_to eq(newURL.total_clicks)
+      end
+    end
+  end
 end
