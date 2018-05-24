35e8c4a8b887bd1c44aad63cb10ad1d9c00c779c
Colin McFadden <cmcfadden@gmail.com>
2018-05-24 09:50:27 -0500

    add model for local lookups

---
+++ app/models/ip_location.rb
@@ -0,0 +1,3 @@
+class IpLocation < ApplicationRecord
+	self.table_name = 'ip2location_db1'
+end
--- db/migrate/20180524122211_add_ip_lookup_table.rb
+++ db/migrate/20180524122211_add_ip_lookup_table.rb
@@ -1,8 +1,8 @@
 class AddIpLookupTable < ActiveRecord::Migration[5.0]
   def change
   	 create_table :ip2location_db1 do |t|
-  	 	t.integer :ip_from
-  	 	t.integer :ip_to
+  	 	t.integer :ip_from, unsigned: true
+  	 	t.integer :ip_to, unsigned: true
   	 	t.column(:country_code, 'char(2)')
   	 	t.string :country_name
   	 	t.index :ip_from
