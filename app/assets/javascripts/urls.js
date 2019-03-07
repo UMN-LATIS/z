127f58dd379908d08ae18a44657abc74827e8697
mcfa0086 <mcfa0086@umn.edu>
2019-03-07 10:51:17 +0600

    Add supporting to admin UI (#515)
    * add custom searches to admin
    
    * maybe only exact matches
    
    * clear search properly

--- app/assets/javascripts/urls.js
+++ app/assets/javascripts/urls.js
@@ -80,7 +80,29 @@ $(document).bind('turbolinks:load', function() {
     if ($("body.admin\\/urls.index").length == 0) {
         return;
     }
-    initializeUrlDataTable(2, "desc", 6, 3, false, false);
+    $('.admin-table.data-table tfoot tr th').each(function () {
+        var title = $(this).text();
+        $(this).html('<input type="text" placeholder="Search ' + title + '" />');
+    });
+    var table = initializeUrlDataTable(2, "desc", 6, 3, false, false);
+    table.columns().every(function () {
+        var that = this;
+
+        $('input', this.footer()).on('keyup change', function () {
+            if (that.search() !== this.value) {
+                that
+                    .search("^" + this.value + "$", true, false, true)
+                    .draw();
+            }
+
+            if(this.value == "") {
+                that
+                    .search("")
+                    .draw();
+            }
+        });
+    });
+
 });
 
 // Turn the group select into the fancy select picker
@@ -377,6 +399,7 @@ function initializeUrlDataTable(sortColumn, sortOrder, actionColumn, keywordColu
         .prependTo('.dataTables_wrapper >.row:eq(0) > .col-sm-6:eq(0)');
 
     $(".col-sm-6 .dt-buttons").removeClass("btn-group");
+    return userTable;
 }
 
 $(document).ready(function() {
