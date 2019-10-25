583b111e9f4d112bc85f6b1b35e180f75a675b20
Colin McFadden <cmcfadden@gmail.com>
2019-10-25 11:43:10 -0500

    erf, lograge docs forgot this bit

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.lograge.keep_original_rails_log = true
-
+  config.lograge.enabled = true
   config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
 end
 
