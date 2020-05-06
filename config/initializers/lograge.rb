0ee3535c31bbf0679e43fffeb0882a54988e6ba0
Colin McFadden <cmcfadden@gmail.com>
2020-05-06 14:49:39 -0500

    lograge change

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -3,5 +3,8 @@ Rails.application.configure do
   config.lograge.enabled = true
   config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
   config.lograge.formatter =  Lograge::Formatters::Json.new
+  config.lograge.custom_options = lambda do |event|
+    { time: Time.now }
+  end
 end
 
