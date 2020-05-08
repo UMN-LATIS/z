75376ce1161f27bf285153eb77eaad9a51fb838e
Colin McFadden <cmcfadden@gmail.com>
2020-05-08 10:49:06 -0500

    another logger tweak

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,7 +1,7 @@
 Rails.application.configure do
   config.lograge.keep_original_rails_log = true
   config.lograge.enabled = true
-  config.lograge.logger = ActiveSupport::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
+  config.lograge.logger = ::Logger.new "#{Rails.root}/log/lograge_#{Rails.env}.log"
   config.lograge.formatter =  Lograge::Formatters::Json.new
   config.lograge.custom_options = lambda do |event|
     { time: Time.now }
