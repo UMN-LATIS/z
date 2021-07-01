203f6515479438dd11ccb0ffd23ebb8fdba3dfbb
jxjj <johnsojr@gmail.com>
2021-07-01 11:52:28 -0500

    fix admin_member_spec tests
    - make hidden `#uid` input visible
    - replace `wait_for_ajax` calls with a `expect` page
    to have "Add an Admin" content
    - change specific uid strings like 'andersen' to a symbol

--- spec/features/admin_members_spec.rb
+++ spec/features/admin_members_spec.rb
@@ -1,14 +1,18 @@
 require 'rails_helper'
 
+def wait_for_admin_page_to_load
+  expect(page).to have_content('Add an Admin')
+end
+
 describe 'admin members index page' do
   before do
-    @user = FactoryBot.create(:user, admin: true, uid: 'wozniak')
+    @user = FactoryBot.create(:user, admin: true, uid: :test_admin_uid)
     sign_in(@user)
     visit admin_members_path(@user)
   end
 
   describe 'visiting the group membership page' do
-    let(:user) { User.where(uid: 'wozniak').first }
+    let(:user) { User.where(uid: :test_admin_uid).first }
     describe 'page content' do
       it 'should display the admin member internet_id' do
         expect(page).to have_content user.internet_id
@@ -21,38 +25,55 @@ describe 'admin members index page' do
 
   describe 'creating and deleting a admin member', js: true do
     describe 'to an existing group of admins' do
+      # Why are we creating a group? It doesn't seem to be used in this test?
       let(:group) { FactoryBot.create(:group) }
+      before do
+        js_make_input_visible('#uid')
+      end
       it 'add and then set the new member admin flag to true' do
-        find('#uid', visible: false).set 'andersen'
+        find('#uid', visible: false).set :new_admin_uid
         click_button 'Add'
         click_button 'Confirm'
-        wait_for_ajax
+        
         visit admin_members_path(@user)
-        expect(User.where(uid: 'andersen', admin: true)).to exist
+        wait_for_admin_page_to_load
+        
+        # DB should be updated
+        expect(User.where(uid: :new_admin_uid, admin: true)).to exist
+        
+        # Admin table should have two entries now.
+        # one for :test_admin_uid and :new_admin_uid
+        within 'table' do
+          expect(page).to have_selector 'tr', count: 2
+        end
+        
       end
       describe 'clicking delete' do
         it 'should first add a new member and then delete it with notification' do
-          find('#uid', visible: false).set 'andersen'
+          find('#uid', visible: false).set :new_admin_uid
           click_button 'Add'
           click_button 'Confirm'
-          wait_for_ajax
+
           visit admin_members_path(@user)
+          wait_for_admin_page_to_load
           page.all(:css, '.delete-admin-member')[1].click
           click_button 'Confirm'
-          wait_for_ajax
+
           visit admin_members_path(@user)
-          expect(User.where(uid: 'andersen', admin: true)).not_to exist
-          expect(page).to have_content '(testinternetid) has been removed.'
+          wait_for_admin_page_to_load
+          expect(User.where(uid: :new_admin_uid, admin: true)).not_to exist
+          expect(page).to have_content 'has been removed.'
         end
       end
       describe 'clicking delete on oneself' do
         it 'should delete oneself and not allow deleted user to view admin pages' do
           visit admin_members_path(@user)
+          wait_for_admin_page_to_load
           page.all(:css, '.delete-admin-member')[0].click
           click_button 'Confirm'
-          wait_for_ajax
+
           visit admin_members_path(@user)
-          expect(User.where(uid: 'wozniak', admin: true)).not_to exist
+          expect(User.where(uid: :test_admin_uid, admin: true)).not_to exist
           expect(page).to have_content 'You are not authorized to perform this action. '
         end
       end
