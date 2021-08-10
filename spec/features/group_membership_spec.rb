19a53b83d692bec5d7179d22e011a85c21f59c8f
James Johnson <980170+jxjj@users.noreply.github.com>
2021-08-10 09:38:45 -0500

    Disable submit button if add new member form is blank (#60)
    This prevents a user from submitting the add new member form if the input is blank.
    
    - Sets a required attribute is set on the input field.
    - Using JS, check that required inputs have some value. If not, the submit button is disabled.
    - Adds tests and fixes a test where the hidden input was set, but not the required input.
    
    Closes #10.

--- spec/features/group_membership_spec.rb
+++ spec/features/group_membership_spec.rb
@@ -1,5 +1,6 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
+require 'rails_helper'
 
 describe 'groups members index page' do
   before do
@@ -8,24 +9,29 @@ describe 'groups members index page' do
 
   describe 'visiting the group membership page' do
     let(:group) { FactoryBot.create(:group) }
-    let(:user) { User.where(:uid => @user.uid).first }
+    let(:user) { User.where(uid: @user.uid).first }
+
     before do
       group.users << @user
       group.save
       sign_in(@user)
       visit group_members_path(group)
     end
+
     describe 'page content' do
-      it 'should display the group name' do
+      it 'displays the group name' do
         expect(page).to have_content group.name
       end
-      it 'should display the group member full name' do
+
+      it 'displays the group member full name' do
         expect(page).to have_content user.display_name
       end
-      it 'should display the group member email' do
+
+      it 'displays the group member email' do
         expect(page).to have_content user.email
       end
-      it 'should display a delete button' do
+
+      it 'displays a delete button' do
         expect(page).to have_content 'Remove'
       end
     end
@@ -34,23 +40,26 @@ describe 'groups members index page' do
   describe 'creating and deleting a group member', js: true do
     describe 'to an existing group' do
       let(:group) { FactoryBot.create(:group) }
+
       before do
         group.users << @user
         group.save
         sign_in(@user)
         visit group_members_path(group)
       end
+
       it 'adding should increase the user count of the group by 1' do
         expect do
           js_make_all_inputs_visible
+          fill_in('Add a new member', with: 'test-user')
           find("#uid", visible: false).set '5scyi59j8'
           click_button 'Add'
           click_button 'Confirm'
           wait_for_ajax
           group.reload
         end.to change(group.users, :count).by(1)
-
       end
+
       it 'clicking delete decrease the user count by one' do
         expect do
           find('.delete-group-member').click
@@ -59,6 +68,21 @@ describe 'groups members index page' do
         end.to change(group.users, :count).by(-1)
       end
 
+      it 'prevents blank submissions' do
+        # by default the button should be disabled
+        # until a value is entered
+        expect(page).to have_button('Add member', disabled: true)
+
+        # enter a value
+        fill_in 'Add a new member to this collection', with: 'johnsojr'
+        # the button should now be enabled
+        expect(page).to have_button('Add member', disabled: false)
+
+        # remove the text
+        fill_in 'Add a new member to this collection', with: ''
+        # the button should now be disabled again
+        expect(page).to have_button('Add member', disabled: true)
+      end
     end
   end
 end
