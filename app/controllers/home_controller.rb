7e568da85c4532ba349226d08cbc8438e0085c50
James Johnson <johnsojr@umn.edu>
2021-08-18 18:17:49 -0500

    Force SSL in config rather than in controllers
    The `force_ssl` method on controllers has been removed in 6.1.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,5 @@
 class HomeController < ApplicationController
   before_action :redirect_to_urls_if_logged_in
-  force_ssl if: :ssl_configured?
-  def index; end
 
-  def ssl_configured?
-    !Rails.env.development?
-  end
+  def index; end
 end
