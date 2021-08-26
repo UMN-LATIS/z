9945f2c017c03d1fca84e03fff576bf500686b88
James Johnson <johnsojr@umn.edu>
2021-08-26 17:01:22 -0500

    Rendering vue component

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,4 +14,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log("Hello World from Webpacker");
+import main from "@/main";
+
+main();
