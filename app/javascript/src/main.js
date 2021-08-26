ff9e1812e54dd9647b9da21173b70d3d8297920b
James Johnson <johnsojr@umn.edu>
2021-08-26 18:38:25 -0500

    Workaround vue + turbolinks + datatables issues

--- app/javascript/src/main.js
+++ app/javascript/src/main.js
@@ -2,7 +2,11 @@ import { createApp } from "vue";
 import TurbolinksAdapter from "vue-turbolinks";
 import HelloWorld from "@/components/HelloWorld.vue";
 
-const app = createApp({ components: { HelloWorld } }).use(TurbolinksAdapter);
+// const app = createApp({ components: { HelloWorld } }).use(TurbolinksAdapter);
+const createMyApp = () =>
+  createApp({ components: { HelloWorld } })
+    .use(TurbolinksAdapter)
+    .mount('[data-behavior="vue"]');
 
 const onReady = (fn) =>
   document.readyState != "loading"
@@ -10,5 +14,27 @@ const onReady = (fn) =>
     : document.addEventListener("DOMContentLoaded", fn);
 
 export default () => {
-  onReady(() => app.mount('[data-behavior="vue"]'));
+  // Datatables and Vue and Turbolinks seem to not play well together
+  // Loading a Vue component using `onReady` will only work for the First
+  // page render as Turbolinks takes over after that.
+  // listening for `turbolinks:load` event seems to interfere with datatables
+  // loading
+  // `turbolinks:before-render` causes Vue not to load.
+  // `turbolinks:render` will be called after every non-initial page load.
+  // SO, we load Vue using onReady and `turbolinks:render` to cover all
+  // cases.
+
+  // My suspicion is that this is a quirk of some legacy event listener
+  // elsewhere in the app. Once that's resolved, Turbolinks probably won't
+  // be so fussy.
+
+  // Called on first page load
+  onReady(() => {
+    createMyApp();
+  });
+
+  // Called after every non-initial page load
+  document.addEventListener("turbolinks:render", () => {
+    createMyApp();
+  });
 };
