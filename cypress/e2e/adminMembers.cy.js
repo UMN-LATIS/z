1f710e0fd525b70079d612bf97ad59b91af9d7ca
James Johnson <johnsojr@umn.edu>
2022-12-20 16:53:07 -0600

    test: non-admins aren't authorized to view admin members page

---
+++ cypress/e2e/adminMembers.cy.js
@@ -0,0 +1,26 @@
+describe("/admin", () => {
+  beforeEach(() => {
+    cy.app("clean");
+  });
+
+  context("as a non-admin", () => {
+    beforeEach(() => {
+      // create a test user without admin privileges
+      cy.appFactories([["create", "user", { uid: "testuser" }]]);
+
+      cy.login({ uid: "testuser" });
+    });
+
+    it("should not be able to access admin members page", () => {
+      cy.visit("/shortener/admin/members");
+
+      // it should redirect to the urls page
+      cy.url().should("eq", Cypress.config().baseUrl + "/shortener/urls");
+
+      // it should display an error message
+      // FIXME: the error message is actually hidden behind the
+      // hero. See #120
+      cy.contains("not authorized", { matchCase: false }).should("be.visible");
+    });
+  });
+});
