004103533d7939d9bf967ac6c202e17be34a3644
James Johnson <johnsojr@umn.edu>
2022-12-20 14:04:21 -0600

    prettier formatting

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,8 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 // import 'cypress-on-rails/support/index'
-import './commands'
-import './on-rails'
+import "./commands";
+import "./on-rails";
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
