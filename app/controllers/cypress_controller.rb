98408e867b64b8b6317953b89fb72db843a1852b
James Johnson <johnsojr@umn.edu>
2023-01-04 21:05:44 -0600

    fix cypress login controller - search by uid

--- app/controllers/cypress_controller.rb
+++ app/controllers/cypress_controller.rb
@@ -2,11 +2,11 @@ class CypressController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def login
-    if params[:email].present?
-      user = User.find_by!(uid: params.require(:uid))
-    else
-      user = User.first!
-    end
+    user = if params[:uid].present?
+             User.find_by!(uid: params.require(:uid))
+           else
+             User.first!
+           end
     sign_in(user)
     redirect_to URI.parse(params.require(:redirect_to)).path
   end
