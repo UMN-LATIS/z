3407c240055e01cdadbab76cfd82b61ee203ab9b
James Johnson <johnsojr@umn.edu>
2023-01-08 01:30:20 -0600

    WIP Url Show Stats Page

--- cypress/e2e/urlShowStatsPage.cy.ts
+++ cypress/e2e/urlShowStatsPage.cy.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 import admin from "../fixtures/users/admin.json";
 import user1 from "../fixtures/users/user1.json";
 import user2 from "../fixtures/users/user2.json";
@@ -6,12 +8,27 @@ describe("admin url details (stats) page", () => {
   beforeEach(() => {
     cy.app("clean");
 
+    // since our DB is not populated with data that converts
+    // ip addresses to locations, the country code for each click
+    // will be null. This causes an error to be thrown when
+    // the google chart is generated. So, we catch this error and ignore it.
+    cy.on("uncaught:exception", (err, runnable) => {
+      if (err.message.includes("google.load is not a function")) {
+        return false;
+      }
+    });
+
     // create a test user without admin privileges
     // and a create url that belong to user1
     cy.createUser(user1.umndid).then((user) => {
       cy.createUrl({
-        keyword: "user1",
-        url: "https://user1url.com",
+        keyword: "cla",
+        url: "https://cla.umn.edu",
+        user,
+      });
+      cy.createUrl({
+        keyword: "morris",
+        url: "https://morris.umn.edu",
         user,
       });
     });
@@ -30,7 +47,7 @@ describe("admin url details (stats) page", () => {
     it("redirects a user who is not the owner or an admin back to the urls page and shows a not authorized error", () => {
       cy.createAndLoginUser("testuser");
 
-      cy.visit("/shortener/urls/user1");
+      cy.visit("/shortener/urls/cla");
 
       // it should redirect to the urls page
       cy.location("pathname").should("eq", "/shortener/urls");
@@ -49,5 +66,59 @@ describe("admin url details (stats) page", () => {
     });
   });
 
-  it("tracks clicks", () => {});
+  context("when url owner is logged in", () => {
+    beforeEach(() => {
+      cy.login(user1.umndid);
+      cy.visit("/shortener/urls/cla");
+    });
+
+    it("shows the url details", () => {
+      // long url
+      cy.contains("https://cla.umn.edu").should("be.visible");
+
+      // keyword
+      cy.contains("cla").should("be.visible");
+
+      // created at
+      cy.contains(dayjs().format("MM/DD/YYYY")).should("be.visible");
+    });
+
+    it("shows the total clicks on this url", () => {
+      // initially the clicks should be 0
+      cy.contains("Historical Click Count")
+        .closest(".panel")
+        .find("table")
+        .contains("Last 24 Hours")
+        .closest("tr")
+        .as("last24row");
+
+      cy.get("@last24row").contains("0 hits").should("be.visible");
+
+      // add some clicks
+
+      cy.clickUrl("cla", 3)
+        .then(() => cy.clickUrl("morris", 2))
+        .then(() => cy.reload());
+
+      // the total clicks for cla should be 3
+      cy.get("@last24row").contains("3 hits").should("be.visible");
+    });
+
+    it("shows the best day for clicks", () => {
+      // set the clock to fixed date 1
+      cy.clickUrl("cla", 3);
+
+      // set the dates of the clicks to be 2020-01-01
+      cy.appEval("Click.update_all(created_at: '2020-01-01')");
+
+      // now add a few more clicks
+      cy.clickUrl("cla", 2);
+
+      // reload the page
+      cy.reload();
+
+      // check that the best day is 01-01-2020
+      cy.contains("Best Day").closest(".panel").contains("January 01 2020");
+    });
+  });
 });
