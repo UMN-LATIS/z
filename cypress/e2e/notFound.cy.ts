d4de2792267b1629aed272fabb2b29703d467828
James Johnson <johnsojr@umn.edu>
2023-01-09 15:11:26 -0600

    handling not found and bad requests

--- cypress/e2e/notFound.cy.ts
+++ cypress/e2e/notFound.cy.ts
@@ -5,7 +5,22 @@ describe("not found", () => {
   });
 
   it("shows 404 page if url has bad encoding in path", () => {
-    cy.visit("/%E0%A4%A", { failOnStatusCode: false });
+    // cy.visit doesn't like bad encoding in the paths
+    // so we have to use cy.request
+    cy.request({
+      method: "GET",
+      url: "/%E0%A4%A",
+      failOnStatusCode: false,
+    }).then((response) => {
+      // expect a 404 page
+      expect(response.headers["content-type"]).to.contain("text/html");
+      expect(response.status).to.eq(404);
+      expect(response.body).to.contain("Not Found");
+    });
+  });
+
+  it("shows Not Found page if query string is malformed", () => {
+    cy.visit("/someplace?alsdkjflakjdf%$$$)$%=", { failOnStatusCode: false });
     cy.contains("404").should("exist");
   });
 });
