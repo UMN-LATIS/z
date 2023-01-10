666f21bb5ff603bd41a7bc7c1f69f8ee8f28c0f1
James Johnson <johnsojr@umn.edu>
2023-01-10 15:25:19 -0600

    complete active_storage:update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_19_171642) do
+ActiveRecord::Schema[6.1].define(version: 2021_07_19_171642) do
 
   create_table "clicks", id: :integer, charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "country_code"
