fd8bb93962e75c49aae538125d5b644518c3320f
James Johnson <johnsojr@umn.edu>
2023-01-09 20:26:08 -0600

    FAQ page tests

---
+++ cypress/e2e/faqPage.cy.ts
@@ -0,0 +1,50 @@
+describe("FAQ Page", () => {
+  beforeEach(() => {
+    cy.app("clean");
+
+    // create a FAQ
+    cy.appFactories([
+      [
+        "create",
+        "frequently_asked_question",
+        {
+          header: "General",
+          question: "What is Z?",
+          answer: "This site is used to shorten URLs.",
+        },
+      ],
+      [
+        "create",
+        "frequently_asked_question",
+        {
+          header: "General",
+          question: "Should I use it?",
+          answer: "Indubitably.",
+        },
+      ],
+    ]);
+    cy.visit("/shortener/faq");
+  });
+
+  it("should render correctly", () => {
+    cy.get("h1").should("contain", "Frequently Asked Questions");
+
+    // a single General header
+    cy.get(".faqHeader").should("have.length", 1).should("contain", "General");
+
+    // two questions
+    cy.get(".panel-heading")
+      .should("have.length", 2)
+      .should("contain", "What is Z?")
+      .should("contain", "Should I use it?");
+
+    // answers should be collapsed
+    cy.get(".panel-body").should("have.length", 2).should("not.be.visible");
+
+    // click on the first question
+    cy.contains("What is Z?").click();
+
+    // answer should be visible
+    cy.contains("This site is used to shorten URLs.").should("be.visible");
+  });
+});
