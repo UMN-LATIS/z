c39e26167d89f9f28b8d2ca24cedb62a54ca596d
James Johnson <johnsojr@umn.edu>
2023-01-10 18:54:29 -0600

    allow external redirects... or, what's the point?

--- app/controllers/redirect_controller.rb
+++ app/controllers/redirect_controller.rb
@@ -6,7 +6,7 @@ class RedirectController < ApplicationController
     if url.nil?
       raise ActiveRecord::RecordNotFound
     else
-      redirect_to(url.url)
+      redirect_to(url.url, allow_other_host: true)
       browser = Browser.new(request.user_agent, accept_language: "en-us")
       url.add_click!(request.remote_ip) unless browser.bot?
     end
