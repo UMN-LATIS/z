3db0fe1285da4eb1019d961958ad7d03659cc0b4
James Johnson <johnsojr@umn.edu>
2023-01-31 14:05:13 -0600

    set umn colors

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,19 @@ module.exports = {
       fontFamily: {
         sans: ["Inter var", ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        "umn-maroon": "#7a0019",
+        "umn-maroon-dark": "#5b0013",
+        "umn-gold": "#ffcc33",
+        "umn-gold-light": "#ffde7a",
+        "umn-neutral-50": "#f9f7f6", // page background color
+        "umn-neutral-100": `#f0efee`, // gray for navbar
+        "umn-neutral-200": `#d5d6d2`,
+        "umn-neutral-500": `#737487`,
+        "umn-neutral-700": `#5a5a5a`, // text color
+        "umn-neutral-800": `#262626`, // active text color
+        "umn-neutral-900": `#1a1a1a`,
+      },
     },
   },
   plugins: [
