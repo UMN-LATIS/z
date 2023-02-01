127bb765262daf3981abf4358d695f7f0c329af4
James Johnson <johnsojr@umn.edu>
2023-01-31 22:43:46 -0600

    watch css in test mode too

--- cypress.config.js
+++ cypress.config.js
@@ -5,7 +5,7 @@ const {
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3000",
+    baseUrl: "http://localhost:5100",
     defaultCommandTimeout: 10000,
     supportFile: "cypress/support/index.ts",
     excludeSpecPattern: "**/examples/**",
