0b7a2c26e26c72bb2fda4e92a7196d0603ad79ba
James Johnson <johnsojr@umn.edu>
2023-02-27 13:51:49 -0600

    ensure user is signed in and authorized to access admin groups

--- app/controllers/admin/groups_controller.rb
+++ app/controllers/admin/groups_controller.rb
@@ -1,9 +1,10 @@
 # controllers/groups_controller.rb
 class Admin::GroupsController < ApplicationController
-  # before_action :set_group, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_signed_in
   before_action :ensure_is_admin
 
   def index
     @groups = Group.not_default
+    authorize @groups unless @groups.nil?
   end
 end
