c8b76635e049fb7ede311a577d59891c6743b636
James Johnson <johnsojr@umn.edu>
2023-03-11 14:17:39 -0600

    remove seperate controller for admin url datatables
    This is just handled within the admin urls controller

--- app/controllers/admin/urls_datatable_controller.rb
+++
@@ -1,9 +0,0 @@
-class Admin::UrlsDatatableController < ApplicationController
-  def index
-    respond_to do |format|
-      format.json do
-        render json: AdminUrlDatatable.new(params, view_context:, current_user:)
-      end
-    end
-  end
-end
