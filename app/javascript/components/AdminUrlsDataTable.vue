38acfb50e04dad00edd79edc1487db80ac32f223
James Johnson <johnsojr@umn.edu>
2023-03-10 22:43:27 -0600

    fix collection owner wrapping

--- app/javascript/components/AdminUrlsDataTable.vue
+++ app/javascript/components/AdminUrlsDataTable.vue
@@ -11,7 +11,7 @@
 
   <DataTable
     ref="table"
-    class="table table-striped table-bordered"
+    class="table table-striped table-bordered admin-urls-data-table"
     :options="options"
     :columns="columns"
     :headers="['Z-link', 'Owner', 'Clicks', 'Created', 'Actions']"
@@ -163,7 +163,7 @@ const columns: DataTableColumnOptions[] = [
     data: "keyword",
     render: (keyword: string, _, row) => {
       return `
-        <div class="tw-flex tw-flex-col">
+        <div class="tw-flex tw-flex-col keyword-col">
           <a href="/shortener/urls/${keyword}">
             <span>${window.location.host}/</span>${keyword}
           </a>
@@ -183,19 +183,26 @@ const columns: DataTableColumnOptions[] = [
 
       // default groups have only one member and should be named for the owner's internet id
       return isDefaultGroup
-        ? `<a href="${peopleSearchHref}">${group_name}</a>`
-        : `<a href="${groupMembersHref}" class="tw-flex tw-gap-1">
-               <!-- user group icon -->
+        ? `<a class="admin-urls-datatable__group-col" title="${group_name}" href="${peopleSearchHref}">${group_name}</a>`
+        : `<a href="${groupMembersHref}"  
+              class="admin-urls-datatable__group-col"
+              title="${group_name}"
+              >
               <svg 
                 data-cy="group-icon"
                 xmlns="http://www.w3.org/2000/svg" 
-                class="tw-h-4 tw-w-4"
+                class="admin-urls-datatable__group-icon"
                 fill="currentColor"
                 stroke-width="1.5"
                 stroke="currentColor"
-                height="48" viewBox="0 96 960 960" width="48"><path d="M38 896v-94q0-35 18-63.5t50-42.5q73-32 131.5-46T358 636q62 0 120 14t131 46q32 14 50.5 42.5T678 802v94H38Zm700 0v-94q0-63-32-103.5T622 633q69 8 130 23.5t99 35.5q33 19 52 47t19 63v94H738ZM358 575q-66 0-108-42t-42-108q0-66 42-108t108-42q66 0 108 42t42 108q0 66-42 108t-108 42Zm360-150q0 66-42 108t-108 42q-11 0-24.5-1.5T519 568q24-25 36.5-61.5T568 425q0-45-12.5-79.5T519 282q11-3 24.5-5t24.5-2q66 0 108 42t42 108ZM98 836h520v-34q0-16-9.5-31T585 750q-72-32-121-43t-106-11q-57 0-106.5 11T130 750q-14 6-23 21t-9 31v34Zm260-321q39 0 64.5-25.5T448 425q0-39-25.5-64.5T358 335q-39 0-64.5 25.5T268 425q0 39 25.5 64.5T358 515Zm0 321Zm0-411Z"/></svg>
+                height="48" viewBox="0 96 960 960" width="48"><path d="M38 896v-94q0-35 18-63.5t50-42.5q73-32 131.5-46T358 636q62 0 120 14t131 46q32 14 50.5 42.5T678 802v94H38Zm700 0v-94q0-63-32-103.5T622 633q69 8 130 23.5t99 35.5q33 19 52 47t19 63v94H738ZM358 575q-66 0-108-42t-42-108q0-66 42-108t108-42q66 0 108 42t42 108q0 66-42 108t-108 42Zm360-150q0 66-42 108t-108 42q-11 0-24.5-1.5T519 568q24-25 36.5-61.5T568 425q0-45-12.5-79.5T519 282q11-3 24.5-5t24.5-2q66 0 108 42t42 108ZM98 836h520v-34q0-16-9.5-31T585 750q-72-32-121-43t-106-11q-57 0-106.5 11T130 750q-14 6-23 21t-9 31v34Zm260-321q39 0 64.5-25.5T448 425q0-39-25.5-64.5T358 335q-39 0-64.5 25.5T268 425q0 39 25.5 64.5T358 515Zm0 321Zm0-411Z"/>
+                </svg>
+                <span>
                 ${group_name}
-              </a>`;
+                </span>
+              </div>
+            </a>
+`;
     },
   },
   {
@@ -240,4 +247,23 @@ const columns: DataTableColumnOptions[] = [
   },
 ];
 </script>
-<style scoped></style>
+<style>
+.admin-urls-datatable__group-col {
+  display: block;
+  max-width: 8rem;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+}
+
+.admin-urls-datatable__group-icon {
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  width: 1rem;
+  height: 1rem;
+  /* need min-width and min-height to prevent icon from shrinking */
+  min-width: 1rem;
+  min-height: 1rem;
+}
+</style>
