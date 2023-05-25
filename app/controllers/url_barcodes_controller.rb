1f56975ce7d450b889470c05d19f7d79983d0776
James Johnson <johnsojr@umn.edu>
2023-05-24 23:07:44 -0500

    change to black and use square block M

--- app/controllers/url_barcodes_controller.rb
+++ app/controllers/url_barcodes_controller.rb
@@ -14,15 +14,15 @@ class UrlBarcodesController < ApplicationController
     qrcode = RQRCode::QRCode.new("#{request.base_url}/#{url_record.keyword}")
 
     # Convert the QR Code to PNG
-    png = qrcode.as_png(size: 400, color: "#7a0019", fill: "white")
+    png = qrcode.as_png(size: 640)
 
     # Convert the PNG data to an image object
     image = MiniMagick::Image.read(png.to_s)
 
     # Load the logo and scale it to a suitable size
-    logo_path = Rails.root.join("app/assets/images/block_m_circle.png")
+    logo_path = Rails.root.join("app/assets/images/block_m_square.png")
     logo = MiniMagick::Image.open(logo_path)
-    logo.resize "100x100"
+    logo.resize "154x154"
 
     # Overlay the logo in the center of the QR code
     result = image.composite(logo, "png") do |c|
