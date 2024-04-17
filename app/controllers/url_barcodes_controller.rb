691b384ae1477816acc2bea8a2f8d5e76e3a7516
James Johnson <johnsojr@umn.edu>
2024-04-17 13:29:50 -0500

    move requires to top
    rather than load on each request, load once per deployment

--- app/controllers/url_barcodes_controller.rb
+++ app/controllers/url_barcodes_controller.rb
@@ -1,12 +1,13 @@
 # controllers/url_barcodes_controller.rb
+require 'barby'
+require 'barby/barcode/qr_code'
+require 'barby/outputter/png_outputter'
+
 class UrlBarcodesController < ApplicationController
   def show
     url = Url.find_by(keyword: params[:url_id])
     if url.present?
       authorize url
-      require 'barby'
-      require 'barby/barcode/qr_code'
-      require 'barby/outputter/png_outputter'
       barcode = Barby::QrCode.new(view_context.full_url(url))
       barcode_png = Barby::PngOutputter.new(barcode)
       barcode_png.xdim = 10
