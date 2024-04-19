aff1b188f2bd45fb7017e816b395762cdde4b8fb
James Johnson <johnsojr@umn.edu>
2024-04-19 18:04:22 -0500

    fix failing test for bad encodings
    need handle_bad_encoding_middleware loaded

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,9 @@ require_relative "boot"
 require 'csv'
 require 'rails/all'
 
+# Handle bad url encodings
+require_relative '../app/middleware/handle_bad_encoding_middleware'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -32,6 +35,7 @@ module Z
 
     config.middleware.insert 0, Rack::UTF8Sanitizer
     config.middleware.use Rack::Deflater
+    config.middleware.insert_before Rack::Runtime, HandleBadEncodingMiddleware
 
     config.exceptions_app = routes
 
