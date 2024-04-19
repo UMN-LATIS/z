50713da3f4e6e0370851a9ef73ce28918acabb30
James Johnson <johnsojr@umn.edu>
2024-04-19 12:11:37 -0500

    config update: application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,8 @@
-require_relative 'boot'
+require_relative "boot"
 
+# CSV is part of standard library, so it needs to be explicitly required
 require 'csv'
 require 'rails/all'
-
-# Handle bad url encodings
-require_relative '../app/middleware/handle_bad_encoding_middleware'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -15,22 +13,26 @@ module Z
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
 
-    # Set default time zone
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
     config.time_zone = 'Central Time (US & Canada)'
-    # config.middleware.insert_before Rack::Runtime, 'InvalidDataInterceptor'
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # disable ip spoofing check, as it looks like users behind badly behaving
+    # proxies will now trigger it due to the f5
     config.action_dispatch.ip_spoofing_check = false
 
     config.middleware.insert 0, Rack::UTF8Sanitizer
     config.middleware.use Rack::Deflater
-    config.middleware.insert_before Rack::Runtime, HandleBadEncodingMiddleware
 
     config.exceptions_app = routes
-
-    # See: https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017
-    config.active_record.yaml_column_permitted_classes = [Symbol, Date, Time]
   end
 end
