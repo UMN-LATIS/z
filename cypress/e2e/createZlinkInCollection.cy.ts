8a4caa911b8ff0749e38cecdc543dc9e9586a03e
James Johnson <johnsojr@umn.edu>
2024-06-04 14:53:57 -0500

    test that url isn't created when user isn't member of group

--- cypress/e2e/createZlinkInCollection.cy.ts
+++ cypress/e2e/createZlinkInCollection.cy.ts
@@ -2,18 +2,26 @@ import user1 from "../fixtures/users/user1.json";
 import { RailsModel } from "../types";
 
 describe("create zlink in collection", () => {
-  let group: RailsModel.Group | null = null;
+  let myGroup: RailsModel.Group | null = null;
+  let notMyGroup: RailsModel.Group | null = null;
 
   beforeEach(() => {
     cy.app("clean");
     cy.createAndLoginUser(user1.umndid);
-    cy.createGroup("testcollection").then((grp) => {
-      group = grp;
-      cy.addUserToGroup(user1.umndid, grp);
-    });
+    cy.createGroup("testcollection")
+      .then((grp) => {
+        myGroup = grp;
+        cy.addUserToGroup(user1.umndid, grp);
+      })
+      .then(() => {
+        cy.createGroup("othercollection");
+      })
+      .then((grp) => {
+        notMyGroup = grp;
+      });
   });
   it("creates a zlink within the collection when on a collection page", () => {
-    cy.visit(`/shortener/urls?collection=${group.id}`);
+    cy.visit(`/shortener/urls?collection=${myGroup.id}`);
 
     // create a zlink on this collection page
     cy.get("#url_url").type("https://example.com");
@@ -23,5 +31,21 @@ describe("create zlink in collection", () => {
     cy.get("#urls-table tbody tr:first-child").within(() => {
       cy.contains("https://example.com").should("exist");
     });
+  });
+
+  it("does not permit creating a zlink in a collection you do not have access to", () => {
+    cy.visit(`/shortener/urls?collection=${notMyGroup.id}`);
+    cy.get("#url_url").type("https://thisshouldntwork.com");
+    cy.contains("Z-Link It").click();
+
+    // verify that the url was not created in the database
+    cy.appEval(`Url.where(url: "https://thisshouldntwork.com").count`).should(
+      "eq",
+      0
+    );
+
+    // cy.contains(
+    //   "You do not have permission to access this collection"
+    // ).should("exist");
   });
 });
