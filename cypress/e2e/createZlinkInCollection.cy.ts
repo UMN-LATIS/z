c0323453ed8ac14a75e0e985ecf87b736a101336
James Johnson <johnsojr@umn.edu>
2024-06-04 15:27:12 -0500

    fix test

--- cypress/e2e/createZlinkInCollection.cy.ts
+++ cypress/e2e/createZlinkInCollection.cy.ts
@@ -1,27 +1,31 @@
 import user1 from "../fixtures/users/user1.json";
+import user2 from "../fixtures/users/user2.json";
 import { RailsModel } from "../types";
 
 describe("create zlink in collection", () => {
-  let myGroup: RailsModel.Group | null = null;
-  let notMyGroup: RailsModel.Group | null = null;
+  let user1Collection: RailsModel.Group | null = null;
+  let user2Collection: RailsModel.Group | null = null;
 
   beforeEach(() => {
     cy.app("clean");
-    cy.createAndLoginUser(user1.umndid);
-    cy.createGroup("testcollection")
+    cy.createUser(user1.umndid, { internet_id_loaded: user1.internet_id });
+    cy.createUser(user2.umndid, { internet_id_loaded: user2.internet_id });
+
+    cy.login(user1.umndid);
+
+    cy.createGroup("user1collection")
       .then((grp) => {
-        myGroup = grp;
+        user1Collection = grp;
         cy.addUserToGroup(user1.umndid, grp);
       })
-      .then(() => {
-        cy.createGroup("othercollection");
-      })
+      .then(() => cy.createGroup("user2collection"))
       .then((grp) => {
-        notMyGroup = grp;
+        user2Collection = grp;
+        cy.addUserToGroup(user2.umndid, grp);
       });
   });
   it("creates a zlink within the collection when on a collection page", () => {
-    cy.visit(`/shortener/urls?collection=${myGroup.id}`);
+    cy.visit(`/shortener/urls?collection=${user1Collection.id}`);
 
     // create a zlink on this collection page
     cy.get("#url_url").type("https://example.com");
@@ -34,18 +38,17 @@ describe("create zlink in collection", () => {
   });
 
   it("does not permit creating a zlink in a collection you do not have access to", () => {
-    cy.visit(`/shortener/urls?collection=${notMyGroup.id}`);
-    cy.get("#url_url").type("https://thisshouldntwork.com");
+    cy.visit(`/shortener/urls?collection=${user2Collection.id}`);
+    cy.get("#url_url").type("https://madeByUser1.com");
     cy.contains("Z-Link It").click();
 
-    // verify that the url was not created in the database
-    cy.appEval(`Url.where(url: "https://thisshouldntwork.com").count`).should(
-      "eq",
-      0
-    );
+    // expect to see the url in User 1's default collection
+    cy.visit("/shortener/urls");
+    cy.contains("https://madeByUser1.com").should("exist");
 
-    // cy.contains(
-    //   "You do not have permission to access this collection"
-    // ).should("exist");
+    // then check the user 2 collection to verify the url is not there
+    cy.login(user2.umndid);
+    cy.visit(`/shortener/urls?collection=${user2Collection.id}`);
+    cy.contains("https://madeByUser1.com").should("not.exist");
   });
 });
