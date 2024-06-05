9fdf0297abbeabd2b5f7fb49bbe87d7638a00b94
James Johnson <johnsojr@umn.edu>
2024-06-05 16:04:05 -0500

    edit form

--- cypress/e2e/urlNotes.cy.ts
+++ cypress/e2e/urlNotes.cy.ts
@@ -13,30 +13,59 @@ describe("create zlink in collection", () => {
     cy.login("testuser");
   });
 
-  it("adds a note to a url", () => {
-    cy.visit("/shortener/urls/cla");
-
-    cy.get("[data-cy='note-form']").within(() => {
-      cy.get("[data-cy='note-input']").type("This is a note");
-      cy.contains("Save").click();
+  context("url show page (aka stats page)", () => {
+    beforeEach(() => {
+      cy.visit("/shortener/urls/cla");
     });
 
-    cy.contains("Note saved").should("be.visible");
+    it("adds a note to a url", () => {
+      cy.get("[data-cy='note-form']").within(() => {
+        cy.get("[data-cy='note-input']").type("This is a note");
+        cy.contains("Save").click();
+      });
 
-    // reload the page
-    cy.visit("/shortener/urls/cla");
-    // check that the note is there
-    cy.get("[data-cy='note-input']").should("contain", "This is a note");
+      cy.contains("Note saved").should("be.visible");
+
+      // reload the page
+      cy.visit("/shortener/urls/cla");
+      // check that the note is there
+      cy.get("[data-cy='note-input']").should("contain", "This is a note");
+    });
+
+    it("limits the note contents to 1000 characters", () => {
+      cy.get("[data-cy='note-form']").within(() => {
+        cy.get("[data-cy='note-input']").type("a".repeat(1001), { delay: 0 });
+        cy.contains("Save").click();
+      });
+
+      cy.contains("Note is too long").should("be.visible");
+    });
   });
 
-  it('limits the note contents to 1000 characters', () => {
-    cy.visit("/shortener/urls/cla");
-
-    cy.get("[data-cy='note-form']").within(() => {
-      cy.get("[data-cy='note-input']").type("a".repeat(1001), { delay: 0});
-      cy.contains("Save").click();
+  context("url index page", () => {
+    beforeEach(() => {
+      cy.visit("/shortener/urls");
     });
 
-    cy.contains("Note is too long").should("be.visible");
-  })
+    it("edits a note", () => {
+      cy.get("#urls-table").contains("cla").closest("tr").as("claRow");
+      cy.get("@claRow").find(".dropdown").as("claDropdown");
+      cy.get("@claDropdown").find(".actions-dropdown-button").click();
+      cy.get("@claDropdown").contains("Edit").click();
+
+      cy.get("[data-cy='note-input']").type("edited note");
+
+      cy.contains("Submit").click();
+
+      // reload the page
+      cy.visit("/shortener/urls");
+
+      // check that the note is there
+      cy.get("#urls-table").contains("cla").closest("tr").as("claRow");
+      cy.get("@claRow").find(".dropdown").as("claDropdown");
+      cy.get("@claDropdown").find(".actions-dropdown-button").click();
+      cy.get("@claDropdown").contains("Edit").click();
+      cy.get("[data-cy='note-input']").should("contain", "edited note");
+    });
+  });
 });
