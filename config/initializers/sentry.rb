3f8fa6dc1d3bbff24b77bb7438568abe1eab24e2
James Johnson <johnsojr@umn.edu>
2024-12-19 11:47:50 -0600

    fix: ci test
    can't we just ignore the sentry_dsn in ci?

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = ENV.fetch("SENTRY_DSN")
+  config.dsn = ENV.fetch("SENTRY_DSN", "")
   config.breadcrumbs_logger = [:active_support_logger]
 
   # Set tracesSampleRate to 1.0 to capture 100%
