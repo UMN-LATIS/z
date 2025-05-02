6eed1cfec0113d5dbd843484e8ed3e1215aa4237
James Johnson <johnsojr@umn.edu>
2025-05-02 23:33:58 +0000

    refactor

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,36 +1,18 @@
-idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
-idp_metadata = idp_metadata_parser.parse_remote_to_hash("https://login.umn.edu/metadata.xml")
+# `saml` or `developer`
+Rails.application.config.omniauth_provider = ENV['OMNIAUTH_PROVIDER'] || 'saml'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
- 
-  provider :saml, {
-    idp_cert_multi: idp_metadata[:idp_cert_multi],
-    uid_field: ENV['SHIB_DID'],
-    extra_fields: [
-      ENV['SHIB_IS_GUEST']
-    ],
-    issuer: "#{Rails.application.config.app_base_url}",
-    assertion_consumer_service_url: ENV['SHIB_ASSERTION_CONSUMER_URL'],
-    sp_entity_id: ENV['SHIB_ENTITY_ID'],
-    idp_sso_service_url: ENV['SHIB_IDP_SSO'],
-    idp_metadata: idp_metadata,
-    certificate: ENV['SHIB_X509_CERT'] || raise("SHIB_X509_CERT environment variable must be defined"),
-    private_key: ENV['SHIB_PRIVATE_KEY'] || raise("SHIB_PRIVATE_KEY environment variable must be defined"),
-    security: {
-      metadata_signed: true,
-      authn_requests_signed: true,
-      signature_method: XMLSecurity::Document::RSA_SHA256,
-    },
-    protocol_binding: OneLogin::RubySaml::Utils::BINDINGS[:post],
-    name_identifier_format: nil
-  } 
+
+    # Configure SAML provider
+    if Rails.application.config.omniauth_provider == 'saml'
+      provider :saml, -> { Rails.application.config.shib_settings }
+    end
+
+    if Rails.application.config.omniauth_provider == 'developer'
+      provider :developer
+    end
 end
 
-
-# Rails.application.config.middleware.use OmniAuth::Builder do
-#   # provider :developer unless Rails.env.production? || Rails.env.staging? || Rails.env.remotedev?
-  
-# end
 
 OmniAuth.config.logger = Rails.logger
 
