1c55bf8946f9fa63a6de90d9ece4fff1cd4a664e
James Johnson <johnsojr@umn.edu>
2025-05-06 13:25:50 -0700

    fix: `/auth/saml/metadata` gives `No Method Error` (#213)

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,32 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.application.config.omniauth_provider == "developer"
     provider :developer
   else
-    provider :saml, Rails.application.config.shib_settings
+    idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+
+    idp_metadata = idp_metadata_parser.parse_remote_to_hash(ENV["SHIB_METADATA_URL"] || raise("SHIB_METADATA_URL environment variable must be defined"))
+
+    provider :saml, {
+      idp_cert_multi: idp_metadata[:idp_cert_multi],
+      uid_field: ENV["SHIB_DID"],
+      extra_fields: [
+        ENV["SHIB_IS_GUEST"]
+      ],
+      issuer: ENV["APP_URL"],
+      assertion_consumer_service_url: ENV["SHIB_ASSERTION_CONSUMER_URL"],
+      sp_entity_id: ENV["SHIB_ENTITY_ID"],
+      idp_sso_service_url: ENV["SHIB_IDP_SSO"],
+      idp_metadata: idp_metadata,
+      certificate: ENV["SHIB_X509_CERT"] || raise("SHIB_X509_CERT environment variable must be defined"),
+      private_key: ENV["SHIB_PRIVATE_KEY"] || raise("SHIB_PRIVATE_KEY environment variable must be defined"),
+      security: {
+        metadata_signed: true,
+        authn_requests_signed: true,
+        digest_method: XMLSecurity::Document::SHA256,
+        signature_method: XMLSecurity::Document::RSA_SHA256
+      },
+      protocol_binding: OneLogin::RubySaml::Utils::BINDINGS[:post],
+      name_identifier_format: nil
+    }
   end
 end
 
