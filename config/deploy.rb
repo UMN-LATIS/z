67b750611fb57471df165692efba4d6fd1f10860
James Johnson <johnsojr@umn.edu>
2025-05-06 18:32:41 +0000

    add .env to capistrano linked files

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :deploy_to, '/swadm/web/z/'
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/ldap.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/ldap.yml','.env'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'public/packs', '.bundle',
