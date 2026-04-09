9c00b9e4ab8675372c6d89d86d7c40d24c118b62
James Johnson <johnsojr@umn.edu>
2026-04-09 20:57:40 +0000

    extract condition as a lambda we can pass to other columns

--- app/datatables/application_datatable.rb
+++ app/datatables/application_datatable.rb
@@ -6,4 +6,17 @@ class ApplicationDatatable < AjaxDatatablesRails::ActiveRecord
     @view = opts[:view_context]
     super
   end
+
+  # Returns a lambda for use as a custom :cond in view_columns.
+  # Wrap a search term in double quotes for an exact match, e.g. "hello".
+  # Without quotes, the default substring (LIKE %term%) match is used.
+  def quoted_or_substring_match
+    ->(column, value) {
+      if value.match?(/\A"(.+)"\z/)
+        column.table[column.field].matches(value.delete_prefix('"').delete_suffix('"'))
+      else
+        column.table[column.field].matches("%#{value}%")
+      end
+    }
+  end
 end
